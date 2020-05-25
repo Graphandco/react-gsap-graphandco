8434a198bd2fcb428901b2dfa1906d6d810c1b7b
Reggio <contact@graphandco.com>
2020-05-25 20:04:24 +0200

    Responsive Nav

--- src/shared/components/Header.js
+++ src/shared/components/Header.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => {
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 768px)' });
     return (
-        <nav>
-            <NavLink to='/' exact activeStyle={{}}>
-                Home
-            </NavLink>
-            <NavLink to='/about' exact activeStyle={{}}>
-                A propos
-            </NavLink>
-            <NavLink to='/tips' exact activeStyle={{}}>
-                Tips
-            </NavLink>
-            <NavLink to='/contact' exact activeStyle={{}}>
-                Contact
-            </NavLink>
-        </nav>
+        <>
+            {isTabletOrMobile && <MenuIcon />}
+            <nav className={isTabletOrMobile && 'responsive'}>
+                <NavLink to='/' exact activeStyle={{}}>
+                    Home
+                </NavLink>
+                <NavLink to='/about' exact activeStyle={{}}>
+                    A propos
+                </NavLink>
+                <NavLink to='/tips' exact activeStyle={{}}>
+                    Tips
+                </NavLink>
+                <NavLink to='/contact' exact activeStyle={{}}>
+                    Contact
+                </NavLink>
+            </nav>
+        </>
     );
 };
 
