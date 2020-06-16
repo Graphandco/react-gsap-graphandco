49a839cd725dca24fc3b436dc87dd0e074c9a991
Reggio <contact@graphandco.com>
2020-06-16 19:38:33 +0200

    IntersectionObserver on progress circles

--- src/about/AboutCompetencesItem.js
+++ src/about/AboutCompetencesItem.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useIntersection } from 'react-use';
 //import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
@@ -11,9 +12,18 @@ import { motion } from 'framer-motion';
 // `;
 
 const AboutCompetencesItem = ({ competences }) => {
+    // Ref for our element
+    const sectionRef = useRef(null);
+    // All the ref to be observed
+    const intersection = useIntersection(sectionRef, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.2,
+    });
+
     const progress = 440 - (440 * `${competences.power}`) / 100;
-    const speedProgress = Math.abs(progress - 200 / 10);
-    console.log(speedProgress);
+    const speedProgress = `${competences.power}` / 30;
+    //console.log(speedProgress);
 
     const circleVariants = {
         hidden: {
@@ -22,36 +32,43 @@ const AboutCompetencesItem = ({ competences }) => {
         visible: {
             strokeDashoffset: progress,
             transition: {
-                duration: 2,
+                duration: speedProgress,
                 ease: 'easeInOut',
             },
         },
     };
 
     return (
-        <div className='progress-bar'>
-            <div className='percent'>
-                <svg>
-                    <circle cx='70' cy='70' r='70'></circle>
-                    <motion.circle
-                        cx='70'
-                        cy='70'
-                        r='70'
-                        style={{
-                            strokeDasharray: 440,
-                            stroke: `${competences.color}`,
-                        }}
-                        variants={circleVariants}
-                        initial='hidden'
-                        animate='visible'
-                    ></motion.circle>
-                </svg>
-                <div className='number'>
-                    <span>{competences.power}%</span>
+        <>
+            <div className='progress-bar' ref={sectionRef}>
+                <div className='percent'>
+                    <svg>
+                        <circle cx='70' cy='70' r='70'></circle>
+                        <motion.circle
+                            cx='70'
+                            cy='70'
+                            r='70'
+                            style={{
+                                strokeDasharray: 440,
+                                stroke: `${competences.color}`,
+                            }}
+                            variants={circleVariants}
+                            initial='hidden'
+                            animate={
+                                intersection &&
+                                intersection.intersectionRatio < 0.2
+                                    ? 'hidden'
+                                    : 'visible'
+                            }
+                        ></motion.circle>
+                    </svg>
+                    <div className='number'>
+                        <span>{competences.power}%</span>
+                    </div>
                 </div>
+                <div className='name'>{competences.name}</div>
             </div>
-            <div className='name'>{competences.name}</div>
-        </div>
+        </>
     );
 };
 
